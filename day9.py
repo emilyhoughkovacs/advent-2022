52d2bbbf71ebf86964cf57570351ed07d95cf17a
Emily Hough-Kovacs <emilyhk@spotify.com>
2022-12-09 17:21:03 -0800

    day 9 solutions... hacked part 2 but whatever

---
+++ day9.py
@@ -0,0 +1,93 @@
+import sys
+import os
+import re
+
+class Solution(object):
+    def __init__(self):
+        self.args = [arg for arg in sys.argv[1:]]
+
+    def solution(self, args):
+
+        # uses first command line arg if exists else inputfile.txt
+        file = args[0] if args and os.path.isfile(args[0]) else 'inputfile.txt'
+        f = open(file, 'r')
+        lines = f.read().splitlines()
+        f.close()
+
+        lines = [line.split(' ') for line in lines]
+        lines = [[line[0], int(line[1])] for line in lines]
+
+        head = [0, 0]
+        tail = [0, 0]
+
+        def moveTail(h=head, t=tail):
+            horizontalDiff = h[0]-t[0]
+            verticalDiff = h[1]-t[1]
+
+            if abs(horizontalDiff) <= 1 and abs(verticalDiff) <= 1 or abs(horizontalDiff) <= 1 and abs(verticalDiff) <= 1:
+                return t
+
+            elif verticalDiff == 0:
+                if horizontalDiff == 2:
+                    return [t[0]+1, t[1]]
+                elif horizontalDiff == -2:
+                    return [t[0]-1, t[1]]
+
+            elif horizontalDiff == 0:
+                if verticalDiff == 2:
+                    return [t[0], t[1]+1]
+                elif verticalDiff == -2:
+                    return [t[0], t[1]-1]
+
+            elif horizontalDiff >= 1 and verticalDiff >= 1:
+                return [t[0]+1, t[1]+1]
+
+            elif horizontalDiff >= 1 and verticalDiff <= -1:
+                return [t[0]+1, t[1]-1]
+
+            elif horizontalDiff <= -1 and verticalDiff >= 1:
+                return [t[0]-1, t[1]+1]
+
+            elif horizontalDiff <= -1 and verticalDiff <= -1:
+                return [t[0]-1, t[1]-1]
+
+        # print(moveTail([2,1], [1,1]))
+        # print(moveTail([1,2], [2,1]))
+        # print(moveTail([1,1], [1,1]))
+        # print(moveTail([3,1], [1,1]))
+        # print(moveTail([1,2], [1,3]))
+        # print(moveTail([1,1], [1,3]))
+        # print(moveTail([2,3], [1,1]))
+        # print(moveTail([3,2], [1,1]))
+
+        tailLocations = [tail]
+
+        for [direction, amount] in lines:
+            for _ in range(amount):
+                if direction == 'R':
+                    head = [head[0]+1, head[1]]
+                    tail = moveTail(head, tail)
+                    tailLocations.append(tail)
+                if direction == 'U':
+                    head = [head[0], head[1]+1]
+                    tail = moveTail(head, tail)
+                    tailLocations.append(tail)
+                if direction == 'L':
+                    head = [head[0]-1, head[1]]
+                    tail = moveTail(head, tail)
+                    tailLocations.append(tail)
+                if direction == 'D':
+                    head = [head[0], head[1]-1]
+                    tail = moveTail(head, tail)
+                    tailLocations.append(tail)
+            # print(tailLocations)
+
+
+        return len(set(tuple(location) for location in tailLocations))
+
+def main():
+    s = Solution()
+    print(s.solution(s.args))
+
+if __name__ == '__main__':
+    main()
